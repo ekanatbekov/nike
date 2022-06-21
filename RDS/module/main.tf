59cf9ebb136ad5a2bade29bb2be3ddd7836c9a3d
ekanatbekov <kanatbekovesen@gmail.com>
2022-06-21 20:33:16 +0000

    second commit

---
+++ RDS/module/main.tf
@@ -0,0 +1,17 @@
+module rds {
+    source = "../"         #local folder
+    region               = "us-east-1"
+    allocated_storage    = 20
+    engine               = "mysql"
+    engine_version       = "5.7"
+    instance_class       = "db.t3.micro"
+    db_name              = "mydb"
+    username             = "foo"
+    publicly_accessible  = true
+    tags = {
+        Name = "main"
+    }
+}
+output endpoint {
+    value = module.rds.endpoint
+}
